e6d6d617575d03de5eebb32b2f8cafcd6c984af2
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2025-01-14 21:15:59 -0500

    reformat code

--- client/src/app/components/NavBar/index.tsx
+++ client/src/app/components/NavBar/index.tsx
@@ -1,26 +1,33 @@
 "use client";
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 const Navbar = () => {
-
   const dispatch = useAppDispatch();
-    const isSidebarCollapsed = useAppSelector((state)=>state.global.isSidebarCollapsed);
-  
-    const toggleSidebar = () => {
-      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-    };
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* Left side */}
       <div className="flex justify-between items-center gap-5">
-        <button className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-                onClick={toggleSidebar}>
-
+        <button
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
           <Menu className="w-4 h-4"></Menu>
         </button>
       </div>
@@ -32,19 +39,22 @@ const Navbar = () => {
         />
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <Bell className="text-gray-500" size={20}></Bell>
-
         </div>
       </div>
       {/* Rigth side */}
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
-            <Bell className="cursor-pointer text-gray-500" size={24}/>
+            <Bell className="cursor-pointer text-gray-500" size={24} />
             <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
               3
             </span>
