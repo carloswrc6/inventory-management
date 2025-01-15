0515b8c291e2f5f9f2294d8241635afd1201d786
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2025-01-15 16:14:42 -0500

    add initial dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import CardPopularProducts from "./CardPopularProducts"
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSumary from "./CardSalesSumary";
+
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+      <CardSalesSumary />
+      <CardPurchaseSummary />
     </div>
-  );  
+  );
 };
 
-export default Dashboard
+export default Dashboard;
