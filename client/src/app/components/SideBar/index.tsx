944fc6bcdfc0e223cf525dad0df52e577e936146
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2025-01-15 23:21:30 -0500

    Refactor: Improvements to Header and Sidebar components

--- client/src/app/components/SideBar/index.tsx
+++ client/src/app/components/SideBar/index.tsx
@@ -1,83 +1,99 @@
-import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
-import { ArchiveIcon, CircleDollarSign, Clipboard, Icon, Image, Layout, LucideIcon, Menu, SlidersHorizontal, User } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client'
 
-interface SideBarLinkProps {
-  href: string;
-  icon: LucideIcon;
-  label: string;
-  isCollapsed: boolean;
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+} from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+interface SidebarLinkProps {
+  href: string
+  icon: LucideIcon
+  label: string
+  isCollapsed: boolean
 }
 
-const SideBarLink = ({
+const SidebarLink = ({
   href,
   icon: Icon,
   label,
   isCollapsed,
-}: SideBarLinkProps) => {
-  const pathname = usePathname();
+}: SidebarLinkProps) => {
+  const pathname = usePathname()
   const isActive =
-    pathname === href || (pathname === "/" && href === "/dashboard");
+    pathname === href || (pathname === '/' && href === '/dashboard')
+
   return (
     <Link href={href}>
       <div
         className={`cursor-pointer flex items-center ${
-          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
-        } 
-    hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
-      isActive ? "bg-blue-200 text-white" : ""
-    } `}
+          isCollapsed ? 'justify-center py-4' : 'justify-start px-8 py-4'
+        }
+        hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isActive ? 'bg-blue-200 text-white' : ''
+        }
+      }`}
       >
         <Icon className="w-6 h-6 !text-gray-700" />
+
         <span
           className={`${
-            isCollapsed ? "hidden" : "block"
+            isCollapsed ? 'hidden' : 'block'
           } font-medium text-gray-700`}
-         >
-        {label}
+        >
+          {label}
         </span>
       </div>
     </Link>
-  );
-};
+  )
+}
 
-const SideBar = () => {
-  const dispatch = useAppDispatch();
+const Sidebar = () => {
+  const dispatch = useAppDispatch()
   const isSidebarCollapsed = useAppSelector(
-    (state) => state.global.isSidebarCollapsed
-  );
+    (state) => state.global.isSidebarCollapsed,
+  )
 
   const toggleSidebar = () => {
-    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
-  };
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  }
 
-  const sideBarClassName = `fixed flex flex-col ${
-    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
-  } 
-  bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+
   return (
-    <div className={sideBarClassName}>
-      {/* Top logo */}
+    <div className={sidebarClassNames}>
+      {/* TOP LOGO */}
       <div
         className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
-          isSidebarCollapsed ? "px-5" : "px-8"
+          isSidebarCollapsed ? 'px-5' : 'px-8'
         }`}
       >
-        <Image
+        {/* <Image
           src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
           alt="edstock-logo"
           width={27}
           height={27}
           className="rounded w-8"
-        />
+        /> */}
         <h1
           className={`${
-            isSidebarCollapsed ? "hidden" : "block"
+            isSidebarCollapsed ? 'hidden' : 'block'
           } font-extrabold text-2xl`}
         >
-          IntMan
+          EDSTOCK
         </h1>
 
         <button
@@ -87,51 +103,53 @@ const SideBar = () => {
           <Menu className="w-4 h-4" />
         </button>
       </div>
-      {/* Links */}
+
+      {/* LINKS */}
       <div className="flex-grow mt-8">
-        <SideBarLink
+        <SidebarLink
           href="/dashboard"
           icon={Layout}
           label="Dashboard"
           isCollapsed={isSidebarCollapsed}
         />
-        <SideBarLink
+        <SidebarLink
           href="/inventory"
-          icon={ArchiveIcon}
+          icon={Archive}
           label="Inventory"
           isCollapsed={isSidebarCollapsed}
         />
-        <SideBarLink
+        <SidebarLink
           href="/products"
           icon={Clipboard}
           label="Products"
           isCollapsed={isSidebarCollapsed}
         />
-        <SideBarLink
+        <SidebarLink
           href="/users"
           icon={User}
           label="Users"
           isCollapsed={isSidebarCollapsed}
         />
-        <SideBarLink
+        <SidebarLink
           href="/settings"
           icon={SlidersHorizontal}
           label="Settings"
           isCollapsed={isSidebarCollapsed}
         />
-        <SideBarLink
+        <SidebarLink
           href="/expenses"
           icon={CircleDollarSign}
           label="Expenses"
           isCollapsed={isSidebarCollapsed}
         />
       </div>
-      {/* Footer */}
-      <div className="flex-grow mt-8">
-        <p className="text-center text-xs text-gray-500">&copy; 2025 Any</p>
+
+      {/* FOOTER */}
+      <div className={`${isSidebarCollapsed ? 'hidden' : 'block'} mb-10`}>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SideBar;
+export default Sidebar
